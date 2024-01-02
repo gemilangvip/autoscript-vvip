4d4aa877fb09ec4b15ea3c2c5d6d81d05248a296
Gemilang Kinasih <154574742+gemilangvip@users.noreply.github.com>
2024-01-02 12:58:15 +0700

    Add files via upload

---
+++ menu/menu-bckp.sh
@@ -0,0 +1,87 @@
+clear
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
+ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
+
+
+echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo -e
+echo -e "${BlueCyan}              Menu Backup                   "
+echo -e
+echo -e "            t.me/RidwanzSaputra"
+echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo
+echo -e "${BlueCyan}Pilih Nomor: "
+echo
+echo "1).Backup"
+echo "2).Restore"
+echo "3).strt"
+echo "4).limit speed"
+echo "5).autobackup"
+echo
+echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo -e "${BlueCyan}"
+read -p    "Pilih Nomor └╼>>>" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+figlet "backup" | lolcat
+backup
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RidwanzSaputra"
+echo
+echo "--------------------------------------------------------"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+figlet "Restore" | lolcat
+restore
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RidwanzSaputra"
+echo
+echo "--------------------------------------------------------"
+fi
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "strt"
+strt
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RidwanzSaputra"
+echo
+echo "--------------------------------------------------------"
+fi
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+limitspeed
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RidwanzSaputra"
+echo
+echo "--------------------------------------------------------"
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+autobackup
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RidwanzSaputra"
+echo
+echo "--------------------------------------------------------"
+fi
---
+++ menu/menu-set.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+# // Exporting Language to UTF-8
+
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+clear
+echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
+echo -e "       ${BIWhite}${UWhite}SETTING ${NC}"
+echo -e ""
+echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Panel Domain      "
+echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Change Port All Account      "
+echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Webmin Menu      "
+echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Speedtest VPS   "
+echo -e "     ${BICyan}[${BIWhite}05${BICyan}] About Script     "
+echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Set Auto Reboot   "
+echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Restart All Service"
+echo -e "     ${BICyan}[${BIWhite}08${BICyan}] Change Banner"
+echo -e "     ${BICyan}[${BIWhite}09${BICyan}] Cek Bandwith"
+echo -e "     ${BICyan}[${BIWhite}10${BICyan}] Setting Auto Reboot"
+
+echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
+echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
+echo ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; menu-domain ; exit ;;
+2) clear ; port-change ; exit ;;
+3) clear ; menu-webmin ; exit ;;
+4) clear ; speedtest ; exit ;;
+5) clear ; about ; exit ;;
+6) clear ; auto-reboot ; exit ;;
+7) clear ; restart ; exit ;;
+8) clear ; nano /etc/issue.net ; exit ;; #ssh-vpn banner.conf
+9) clear ; bw ; exit ;;
+10) clear ; jam ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; menu-set ;;
+esac
