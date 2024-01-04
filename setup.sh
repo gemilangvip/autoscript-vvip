a444c9545ed9cb20e1c2f6a34f60be2e6b591474
Gemilang Kinasih <154574742+gemilangvip@users.noreply.github.com>
2024-01-04 21:10:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -278,8 +278,8 @@ echo "" | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenVPN		: 2086"  | tee -a log-install.txt
 echo "   - OpenSSH		: 22"  | tee -a log-install.txt
-echo "   - SSH Websocket	: 80,8080 [ON]" | tee -a log-install.txt
-echo "   - SSH SSL Websocket	: 443" | tee -a log-install.txt
+echo "   - SSH Websocket	 : 80,8080 [ON]" | tee -a log-install.txt
+echo "   - SSH SSL Websocket : 443" | tee -a log-install.txt
 echo "   - Stunnel4		: 8880, 8443" | tee -a log-install.txt
 echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
 echo "   - Badvpn		: 7100-7900" | tee -a log-install.txt
@@ -297,9 +297,9 @@ echo "   - Timezone		: Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
 echo "   - Fail2Ban		: [ON]"  | tee -a log-install.txt
 echo "   - Dflate		: [ON]"  | tee -a log-install.txt
 echo "   - IPtables		: [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot		: [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot	: [ON]"  | tee -a log-install.txt
 echo "   - IPv6			: [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On	: $aureb:00 $gg GMT +7" | tee -a log-install.txt
+echo "   - Autoreboot On    : $aureb:00 $gg GMT +7" | tee -a log-install.txt
 echo "   - AutoKill Multi Login User" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
 echo "   - Fully automatic script" | tee -a log-install.txt
