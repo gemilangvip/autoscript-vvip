24870817d2e0002e08893ac89824c9d0ddaec5b6
Gemilang Kinasih <154574742+gemilangvip@users.noreply.github.com>
2024-01-06 15:40:08 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -3,10 +3,8 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
-
-
 clear
-source /var/lib/scrz-prem/ipvps.conf
+source /var/lib/SIJA/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
@@ -15,18 +13,18 @@ fi
 tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-echo -e "\033[0;34m┌─────────────────────────────────────────────────┐\033[0m"
-echo -e "\E[44;1;39m              Create Xray/Vless Account            \E[0m"
-echo -e "\033[0;34m└─────────────────────────────────────────────────┘\033[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[40;1;37m       Add Xray/Vless Account      \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
-		read -rp "User: " -e user
+		read -rp "User : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
-clear
-		echo -e "\033[0;34mт┌─────────────────────────────────────────────────┐\033[0m"
-		echo -e "\E[44;1;39m              Create Xray/Vless Account             \E[0m"
-		echo -e "\033[0;34mт└─────────────────────────────────────────────────┘\033[0m"
+		clear
+		echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo -e "\E[40;1;37m       Add Xray/Vless Account      \E[0m"
+		echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
 			echo ""
@@ -36,44 +34,44 @@ clear
 	done
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (days): " masaaktif
-read -p "Limit User (GB): " Quota
+read -p "limit IP ( DEVIC ) : " Limit
+read -p "Limit Quota ( GB ) : " quota
+read -p "Limit Bandwith ( GB ) : " bw
+read -p "Expired (Days) : " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#vless$/a\### '"$user $exp"'\
+sed -i '/#vless$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#vlessgrpc$/a\### '"$user $exp"'\
+sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 systemctl restart xray
 clear
-echo -e "${BIBlue}═══════════XRAY/VLESS═══════════${NC}"
-echo -e "${BIBlue}════════════════════════════════${NC}"
-echo -e "Remarks       : ${user}" 
-echo -e "User Quota    : ${Quota} GB" 
-echo -e "Expired On    : $exp" 
-echo -e "Domain        : ${domain}" 
-echo -e "port TLS      : $tls" 
-echo -e "port none TLS : $none"
-echo -e "port GRPC     : $tls" 
-echo -e "id            : ${uuid}"
-echo -e "Encryption    : none" 
-echo -e "Network       : ws" 
-echo -e "Path          : /vless" 
-echo -e "Path          : vless-grpc"  
-echo -e "${BIBlue}════════════════════════════════${NC}"
-echo -e "Link TLS :"
-echo -e "${vlesslink1}" 
-echo -e "${BIBlue}════════════════════════════════${NC}"   
-echo -e "Link none TLS : "
-echo -e "${vlesslink2}" 
-echo -e "${BIBlue}════════════════════════════════${NC}"
-echo -e "Link GRPC : "
-echo -e "${vlesslink3}" 
-echo -e "${BIBlue}════════════════════════════════${NC}" 
-echo -e "${BICyan}Premium VPN Auto Script Service${NC}" 
-echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[40;1;37m         Xray/Vless Account        \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Remarks        : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Limit ip       : ${Limit} Devic"
+echo -e "Limit Quota    : ${quota} GB"
+echo -e "Limit Bw       : ${bw} GB"
+echo -e "Domain         : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "port TLS       : $tls" | tee -a /etc/log-create-user.log
+echo -e "port none TLS  : $none" | tee -a /etc/log-create-user.log
+echo -e "id             : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Encryption     : none" | tee -a /etc/log-create-user.log
+echo -e "Network        : ws" | tee -a /etc/log-create-user.log
+echo -e "Path           : /vless" | tee -a /etc/log-create-user.log
+echo -e "Path           : vless-grpc" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link GRPC      : ${vlesslink3}" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Expired On     : $exp" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
-
 menu
