94d2a6ecd9e76a0f926418540888bc0ad690dcd8
Gemilang Kinasih <154574742+gemilangvip@users.noreply.github.com>
2024-01-08 18:35:32 +0700

    Update del-tr.sh

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -8,55 +8,63 @@
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # Gemilangkinasih࿐
 
-RED="\033[31m"
-YELLOW="\033[33m"
-NC='\e[0m'
-YELL='\033[0;33m'
-BRED='\033[1;31m'
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+RED='\033[0;31m'
+NC='\033[0m'
 GREEN='\033[0;32m'
-ORANGE='\033[33m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
 BGWHITE='\e[0;100;37m'
-
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "${BGWHITE}         Hapus Akun Trojan         ${NC}"
-        echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		echo ""
-		echo "Anda Tidak Memiliki Member Trojan"
-		echo ""
-		echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		read -n 1 -s -r -p "Press any key to back"
-        m-trojan
+		echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "${BGWHITE}         Delete Akun Trojan        ${NC}"
+        echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo -e ""
+		echo -e "Anda Tidak Memiliki Member Vless!"
+		echo -e ""
+		echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		read -n 1 -s -r -p "Press any key to back on menu"
+        menu
 	fi
 
 	clear
-	echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo -e "${BGWHITE}         Hapus Akun Trojan         ${NC}"
-    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+	echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "${BGWHITE}         Delete Akun Trojan        ${NC}"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e "USER          EXPIRED  " 
-	echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+	echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	grep -e "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo -e ""
-    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	read -rp "Ketik Usernamenya : " user
+    red "Tap Enter To Go Back"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+	read -rp "Input Username : " user
     if [ -z $user ]; then
-    m-trojan
+    menu
     else
     exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
-    sed -i "/### $user $exp/,/^},{/d" /etc/trojan/.trojan.db
+    sed -i "/^#! $user $exp/,/^},{/d" /etc/trojan/.trojan.db
     rm -rf /etc/trojan/$user
     rm -rf /etc/kyt/limit/trojan/ip/$user
     systemctl restart xray > /dev/null 2>&1
     clear
-    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e "» Akun Trojan Berhasil Di Hapus Sayang!"
-    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e "Client Name : $user"
     echo -e "Expired On  : $exp"
-    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e ""
     read -n 1 -s -r -p "Press Any Key To Back"
     menu
