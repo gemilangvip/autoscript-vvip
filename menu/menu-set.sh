27f80c743fa72f99818664c5ded28000fad6bba1
Gemilang Kinasih <154574742+gemilangvip@users.noreply.github.com>
2024-01-09 19:50:49 +0700

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -54,6 +54,7 @@ echo -e "     ${BICyan}[${BIWhite}08${BICyan}] Change Banner"
 echo -e "     ${BICyan}[${BIWhite}09${BICyan}] Cek Bandwith"
 echo -e "     ${BICyan}[${BIWhite}10${BICyan}] Setting Auto Reboot"
 echo -e "     ${BICyan}[${BIWhite}11${BICyan}] Check Log Create"
+echo -e "     ${BICyan}[${BIWhite}12${BICyan}] Instalasi UDPGW Mini"
 
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
@@ -72,6 +73,30 @@ case $opt in
 9) clear ; bw ; exit ;;
 10) clear ; jam ; exit ;;
 11) clear ; cat /etc/log-create-user.log ; exit ;;
+12) clear ;
+echo -e "Proses Instalasi UDPGW Mini 7100, 7200, 7300"; sleep 5;
+
+mkdir -p /usr/local/kyt/
+wget -q -O /usr/local/kyt/udp-mini "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini"
+chmod +x /usr/local/kyt/udp-mini
+wget -q -O /etc/systemd/system/udp-mini-1.service "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini-1.service"
+wget -q -O /etc/systemd/system/udp-mini-2.service "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini-2.service"
+wget -q -O /etc/systemd/system/udp-mini-3.service "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini-3.service"
+systemctl disable udp-mini-1
+systemctl stop udp-mini-1
+systemctl enable udp-mini-1
+systemctl start udp-mini-1
+systemctl disable udp-mini-2
+systemctl stop udp-mini-2
+systemctl enable udp-mini-2
+systemctl start udp-mini-2
+systemctl disable udp-mini-3
+systemctl stop udp-mini-3
+systemctl enable udp-mini-3
+systemctl start udp-mini-3
+
+echo -e "Instalasi SUCCESS UDPGW Mini 7100, 7200, 7300"; sleep 5;
+exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; menu-set ;;
