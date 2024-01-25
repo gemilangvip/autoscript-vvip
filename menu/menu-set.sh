705925f10e7593ce2408889bdc4cf487b523d107
Gemilang Kinasih <154574742+gemilangvip@users.noreply.github.com>
2024-01-25 14:26:55 +0700

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -74,14 +74,14 @@ case $opt in
 10) clear ; jam ; exit ;;
 11) clear ; cat /etc/log-create-user.log ; exit ;;
 12) clear ;
-echo -e "Proses Instalasi UDPGW Mini 7100, 7200, 7300"; sleep 5;
+echo -e "PROSES INSTALASI UDPGW MINI 7100, 7200, 7300"; sleep 5;
 
 mkdir -p /usr/local/kyt/
-wget -q -O /usr/local/kyt/udp-mini "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini"
+wget -q -O /usr/local/kyt/udp-mini "https://raw.githubusercontent.com/gemilangvip/autoscript-vvip/main/udpcustom/udp-mini"
 chmod +x /usr/local/kyt/udp-mini
-wget -q -O /etc/systemd/system/udp-mini-1.service "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini-1.service"
-wget -q -O /etc/systemd/system/udp-mini-2.service "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini-2.service"
-wget -q -O /etc/systemd/system/udp-mini-3.service "https://raw.githubusercontent.com/gemilangvip/autoscript/main/files/udp-mini-3.service"
+wget -q -O /etc/systemd/system/udp-mini-1.service "https://raw.githubusercontent.com/gemilangvip/autoscript-vvip/main/udpcustom/udp-mini-1.service"
+wget -q -O /etc/systemd/system/udp-mini-2.service "https://raw.githubusercontent.com/gemilangvip/autoscript-vvip/main/udpcustom/udp-mini-2.service"
+wget -q -O /etc/systemd/system/udp-mini-3.service "https://raw.githubusercontent.com/gemilangvip/autoscript-vvip/main/udpcustom/udp-mini-3.service"
 systemctl disable udp-mini-1
 systemctl stop udp-mini-1
 systemctl enable udp-mini-1
@@ -95,7 +95,7 @@ systemctl stop udp-mini-3
 systemctl enable udp-mini-3
 systemctl start udp-mini-3
 
-echo -e "Instalasi SUCCESS UDPGW Mini 7100, 7200, 7300"; sleep 5;
+echo -e "INSTALASI SUCCESS UDPGW MINI 7100, 7200, 7300"; sleep 5;
 exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
